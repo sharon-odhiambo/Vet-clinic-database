5a8a7ad2d7d70bc65493872052e1bf3ba921710f
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-09-22 09:26:27 +0300

    upate animals table using transactions and aggregates

--- schema.sql
+++ schema.sql
@@ -6,5 +6,7 @@ CREATE TABLE animals (
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
-    weight_kg decimal
+    weight_kg decimal,
 );
+
+ALTER TABLE animals ADD location text;
